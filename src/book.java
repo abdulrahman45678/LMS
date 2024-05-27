5821072b2678ba0195c497fbfee5dd99628ced61
abdulrahman45678 <abdulrahman040302@gmail.com>
2024-05-27 11:24:51 +0530

    my commit 2

---
+++ src/book.java
@@ -0,0 +1,9 @@
+public class book {
+
+    public static void main(String[] args) {
+
+        System.out.println("book_name");
+        System.out.println("book_number");
+        System.out.println("book_colour");
+    }
+}
---
+++ src/library_system.java
@@ -0,0 +1,9 @@
+public class library_system {
+
+    public static void main(String[] args) {
+
+        System.out.println("librarian");
+        System.out.println("library_name");
+        System.out.println("library_admin");
+    }
+}
